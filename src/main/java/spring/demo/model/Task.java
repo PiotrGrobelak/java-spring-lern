88179fa8063eb078c36593e23123993f4f9eec0d
PiotrGrobelak <grobelak.piotr@gmail.com>
2022-07-07 21:34:23 +0200

    Test API REST with spring

--- src/main/java/spring/demo/model/Task.java
+++ src/main/java/spring/demo/model/Task.java
@@ -1,17 +1,18 @@
 package spring.demo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "tasks")
 public class Task {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String decription;
     private boolean done;
+
+    Task() {}
 
     public int getId() {
         return id;
--- src/main/java/spring/demo/model/TaskRepository.java
+++ src/main/java/spring/demo/model/TaskRepository.java
@@ -1,7 +1,9 @@
 package spring.demo.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 interface TaskRepository extends JpaRepository<Task, Integer> {
 
 }
